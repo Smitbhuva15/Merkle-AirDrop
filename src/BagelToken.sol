e6d0e2a0ad1704d0ae3efc8ecfde33469942bcae
Smitbhuva15 <itssmit123@gmail.com>
2025-06-14 10:37:09 +0000

    Create BagelToken

---
+++ src/BagelToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract BagelToken is ERC20, Ownable {
+    constructor() ERC20("bagel", "BAGEL") Ownable(msg.sender){}
+    /**
+     * @dev Mints `amount` tokens to the `to` address.
+     * Can only be called by the owner of the contract.
+     *
+     * @param to The address to mint tokens to.
+     * @param amount The amount of tokens to mint.
+     */
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
