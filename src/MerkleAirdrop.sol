16103a6258ef164f79aaeb6f514a742e01d0821f
Smitbhuva15 <itssmit123@gmail.com>
2025-06-15 13:49:34 +0000

    setup Merkle root

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,7 +1,24 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.24;
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
 
 contract MerkleAirdrop {
-    
+
+    address [] public claimers;
+    bytes32 private immutable i_merkleRoot;
+    IERC20 private immutable i_airDropToken;
+
+    constructor(bytes32 merkleRoot, IERC20 airDropToken) {
+        i_merkleRoot = merkleRoot;
+        i_airDropToken = airDropToken;
+    }
+
+
+
+
+
+
 }
