961076e1735c769250aa2cc6fac1cfc98efe09be
Smitbhuva15 <itssmit123@gmail.com>
2025-06-17 05:26:28 +0000

    claim Feature added

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.24;
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import {IERC20,SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 
 contract MerkleAirdrop {
@@ -11,9 +11,33 @@ contract MerkleAirdrop {
     bytes32 private immutable i_merkleRoot;
     IERC20 private immutable i_airDropToken;
 
+    using SafeERC20 for IERC20;
+
+
+    //////////////////////      errors         //////////////////////
+
+    error MerkleAirdrop__InvalidProof();
+
+    /////////////////////      events         //////////////////////
+
+    event Claimed(address indexed account, uint256 amount);
+
     constructor(bytes32 merkleRoot, IERC20 airDropToken) {
         i_merkleRoot = merkleRoot;
         i_airDropToken = airDropToken;
+    }
+
+    function claim(address account,uint256 amount,bytes32[] calldata merkleProof) external{
+        // calculate the leaf node hash with the account and amount
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+
+        if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+            revert MerkleAirdrop__InvalidProof();
+        }
+ 
+        // transfer the tokens to the account
+        emit Claimed(account, amount);
+        i_airDropToken.safeTransfer(account, amount);
     }
 
 
